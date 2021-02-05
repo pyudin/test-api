a1f012b274134f9aef35e0b33dfc9576226884d5
Pavel Yudin <pasha.judin@gmail.com>
2021-02-05 16:24:52 +0200

    api upd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use("/", express.static("public"));
-app.use("/users", users);
+app.use("/api/users", users);
 
 app.listen(process.env.PORT || 8090, () =>
   console.log("App is running on port 8090")
